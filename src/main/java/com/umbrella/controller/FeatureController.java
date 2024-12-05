102645cc8c381eea58bec27b2c8d28d384407d96
Armando Jorge Lopez Espinoza <alopeze@proveedor.intercorp.com.pe>
2024-12-05 08:26:38 -0500

    create, edit and delete features

--- src/main/java/com/umbrella/controller/FeatureController.java
+++ src/main/java/com/umbrella/controller/FeatureController.java
@@ -1,11 +1,13 @@
 package com.umbrella.controller;
 
+import com.umbrella.dto.request.FeatureRequestDto;
 import com.umbrella.dto.response.FeatureResponseDto;
+import com.umbrella.exception.ResourceNotFoundException;
 import com.umbrella.service.IFeatureService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +20,26 @@ public class FeatureController {
     @GetMapping()
     public List<FeatureResponseDto> getFeatures(){
         return featureService.getFeatures();
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<String> editFeature(
+            @PathVariable Integer id,
+            @RequestBody FeatureRequestDto feature){
+        featureService.updateFeature(id,feature);
+        return ResponseEntity.ok("{\"message\": \"Feature updated successfully\"}");
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteFeature(@PathVariable Integer id) throws ResourceNotFoundException {
+        featureService.deleteFeature(id);
+        return ResponseEntity.ok("{\"message\": \"Feature deleted successfully\"}");
+    }
+    @PostMapping
+    public ResponseEntity<FeatureResponseDto> createFeature(@RequestBody FeatureRequestDto feature){
+        FeatureResponseDto featureReturn = featureService.createFeature(feature);
+        if(featureReturn==null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.CREATED).body(featureReturn);
+        }
     }
 }
--- src/main/java/com/umbrella/repository/EventFeatureRepository.java
+++ src/main/java/com/umbrella/repository/EventFeatureRepository.java
@@ -1,10 +1,18 @@
 package com.umbrella.repository;
 
 import com.umbrella.entity.EventFeature;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface EventFeatureRepository extends JpaRepository<EventFeature, Integer>{
     List<EventFeature> findByEventId(Integer eventId);
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM EventFeature ef WHERE ef.feature.id = :featureId")
+    void deleteByFeatureId(@Param("featureId") Integer featureId);
 }
